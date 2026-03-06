53b7ba12d7c8593be23b9b85046a10df3f623fc6
eduardolluis <1132907@est.intec.edu.do>
2026-03-06 10:31:09 -0400

    dynamic widget rendering with listview builder

--- lib/customUI/custom_card.dart
+++ lib/customUI/custom_card.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:wazz_up/model/chat_model.dart';
+import 'package:wazz_up/screens/individual_page.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({super.key});
+  const CustomCard({super.key, required this.chatModel});
+  final ChatModel chatModel;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => IndividualPage()),
+        );
+      },
       child: Column(
         children: [
           ListTile(
@@ -15,24 +23,21 @@ class CustomCard extends StatelessWidget {
               radius: 30,
               backgroundColor: Colors.blueGrey,
               child: SvgPicture.asset(
-                "assets/groups.svg",
+                chatModel.isGroup ? "assets/groups.svg" : "assets/person.svg",
                 colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
                 height: 37,
                 width: 37,
               ),
             ),
-            title: Text(
-              "Dev Stack",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
+            title: Text(chatModel.name),
             subtitle: Row(
               children: [
                 Icon(Icons.done_all),
                 SizedBox(width: 3),
-                Text('Hi dev stack', style: TextStyle(fontSize: 13)),
+                Text(chatModel.currentMessage, style: TextStyle(fontSize: 13)),
               ],
             ),
-            trailing: Text("18:04"),
+            trailing: Text(chatModel.time),
           ),
           Padding(
             padding: const EdgeInsets.only(right: 20, left: 80),
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wazz_up/customUI/custom_card.dart';
+import 'package:wazz_up/model/chat_model.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -9,6 +10,43 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatModel> chats = [
+    ChatModel(
+      name: 'Eduardo',
+      icon: 'person.svg',
+      isGroup: false,
+      time: '18:04',
+      currentMessage: 'Hi there',
+    ),
+    ChatModel(
+      name: 'Marcos',
+      icon: 'person.svg',
+      isGroup: false,
+      time: '20:07',
+      currentMessage: 'how you feeling?',
+    ),
+    ChatModel(
+      name: 'David',
+      icon: 'person.svg',
+      isGroup: false,
+      time: '14:13',
+      currentMessage: 'how you feeling?',
+    ),
+    ChatModel(
+      name: 'Elian',
+      icon: 'groups.svg',
+      isGroup: true,
+      time: '15:16',
+      currentMessage: 'hows everyone  feeling?',
+    ),
+    ChatModel(
+      name: 'erik',
+      icon: 'groups.svg',
+      isGroup: true,
+      time: '17:07',
+      currentMessage: 'hey elian',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +54,12 @@ class _ChatPageState extends State<ChatPage> {
         onPressed: () {},
         child: Icon(Icons.chat),
       ),
-      body: ListView(children: [CustomCard(), CustomCard()]),
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index) {
+          return CustomCard(chatModel: chats[index]);
+        },
+      ),
     );
   }
 }
