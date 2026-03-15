083a80f3eb40d6c05f967447a5eed3d4de248150
eduardolluis <1132907@est.intec.edu.do>
2026-03-15 11:52:51 -0400

    update routes

--- chat_server/index.js
+++ chat_server/index.js
@@ -1,28 +1,38 @@
 const express = require("express");
 let http = require("http");
+
 const app = express();
 const port = process.env.PORT || 5000;
 let server = http.createServer(app);
 let io = require("socket.io")(server);
 
-//middleware
-
 app.use(express.json());
+
 let clients = {};
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  console.log(socket.id, "has joined  ");
+  console.log(socket.id, "has joined ");
+
   socket.on("signin", (id) => {
     console.log(id);
     clients[id] = socket;
     console.log(clients);
   });
+
   socket.on("message", (msg) => {
     console.log(msg);
     let targetId = msg.targetId;
     if (clients[targetId]) clients[targetId].emit("message", msg);
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/check", (req, res) => {
+  res.json("Your app is working fine");
 });
 
 server.listen(port, "0.0.0.0", () => {
